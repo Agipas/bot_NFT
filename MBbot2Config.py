c513ec8da16830656745d5802d67986169571b3f
i1 <ivansapiga1999@gmail.com>
2021-11-24 18:56:02 +0200

    Update MBbot2Config.py

--- MBbot2Config.py
+++ MBbot2Config.py
@@ -2,4 +2,4 @@
 requestsNumber = 800
 saleTime =1637578797
 js ={"number":1,"productId":158036705914976256}
-path = r'D:\trade\nftBot\chromedriver.exe'
+path = r'C:\Users\Administrator\Desktop\bot_NFT\chromedriver.exe'
