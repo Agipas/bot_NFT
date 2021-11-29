224f42f0e94a1b314e9c429b7709f7b7c718eb75
i1 <ivansapiga1999@gmail.com>
2021-11-29 18:59:41 +0200

    Create go.bat

---
+++ go.bat
@@ -0,0 +1,13 @@
+C:\Users\Administrator\AppData\Local\Programs\Python\Python310\python.exe -m pip install --upgrade pip
+timeout /t 01
+set PATH=%PATH%;C:\Users\Administrator\AppData\Local\Programs\Python\Python310\Scripts
+timeout /t 01
+pip install selenium
+timeout /t 01
+pip install selenium-wire
+timeout /t 01
+pip install aiohttp
+timeout /t 01
+pip install requests
+timeout /t 01
+pip install packaging
