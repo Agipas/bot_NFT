e37a9f0881fd724d0869ede65f355e1135993066
i1 <ivansapiga1999@gmail.com>
2021-11-30 01:30:05 +0200

    add_bat
    download rep from powershell

--- start.bat
+++ start.bat
@@ -1,13 +1,9 @@
+ren C:\Users\Administrator\Desktop\bot_NFT-main bot_NFT
 C:\Users\Administrator\AppData\Local\Programs\Python\Python310\python.exe -m pip install --upgrade pip
-timeout /t 01
 set PATH=%PATH%;C:\Users\Administrator\AppData\Local\Programs\Python\Python310\Scripts
-timeout /t 01
 pip install selenium
-timeout /t 01
 pip install selenium-wire
-timeout /t 01
 pip install aiohttp
-timeout /t 01
 pip install requests
-timeout /t 01
 pip install packaging
+
